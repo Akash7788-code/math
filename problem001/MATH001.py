ba8062b2d5014868ef2361c41c415f779dd7bd23
Akash7788-code <akssh.kunjir@techsamsoh.com>
2025-02-02 19:01:41 +0530

    problrem no 4

--- problem001/MATH001.py
+++
@@ -1,8 +0,0 @@
-def sum_of_multiples(n, x):
-    m = (n - 1) // x  
-    return x * m * (m + 1) // 2  
-
-n = 1000
-result = sum_of_multiples(n, 3) + sum_of_multiples(n, 5) - sum_of_multiples(n, 15) 
-
-print(result)  
